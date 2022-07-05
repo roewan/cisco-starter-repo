a5fab19cf1bbac6146a970ef7f9acf54f8ffbb19
Roefatin Ridzuan <roefatinridzuan@gmail.com>
2022-07-05 23:03:51 +0800

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
-    <div className="Roe's App">
+    <div className="App-link">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
